2dd4301e80dca42e41394d7ddfb94fff2af54364
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-03-09 18:07:51 +0530

    navbar styling with design done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css'
 import { Footer, Header, Blog, Possibility, Features, WhatGPT3} from './container'
 import { Cta, Brand, Navbar } from './components'
 
+// figma link: https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A15 
+
 const App = () => {
   return (
     <div className="App">
