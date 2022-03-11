05acf351d007aeb4a277445f9b3285d9b2710a0c
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-03-11 16:33:56 +0530

    alt tag added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,19 +5,19 @@ import './brand.css';
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div>
-      <img src={google} />
+      <img src={google} alt="company" />
     </div>
     <div>
-      <img src={slack} />
+      <img src={slack} alt="company" />
     </div>
     <div>
-      <img src={atlassian} />
+      <img src={atlassian} alt="company" />
     </div>
     <div>
-      <img src={dropbox} />
+      <img src={dropbox} alt="company" />
     </div>
     <div>
-      <img src={shopify} />
+      <img src={shopify} alt="company" />
     </div>
   </div>
 );
